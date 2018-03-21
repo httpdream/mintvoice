e9115bc0c6cf2e05ef4b986b00c34ed491e13637
httpdream <httpdream.kay@gmail.com>
2018-03-21 23:48:13 +0900

    Add file upload

--- app.ts
+++ app.ts
@@ -1,8 +1,21 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as multer from "multer";
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
 import { indexRouter } from "./routes/index";
 
 const app: express.Express = express();
+
+let upload = multer({ storage: storage });
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
@@ -12,4 +25,7 @@ app.use(express.static("workspace"));
 
 app.use("/", indexRouter);
 
+app.post("/upload", upload.single("sampleFile"), (req, res, next) => {
+  res.send(req.file);
+});
 export { app };
