9bf1fc532f9783e2e09551e9e625aac473cf7d4d
httpdream <httpdream.kay@gmail.com>
2018-03-26 10:55:32 +0900

    Fix main

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,39 @@ import boardRouter from "./board";
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res, next) => {
-  res.send("hello world");
+  console.log("hihi");
+  sql.exec(`
+  SELECT *
+  FROM tag`)
+  .then (rows => {
+    if (rows.length === 0) {
+      return res.render("../workspace/main.html");
+    }
+
+    let tags = {
+      category: [],
+      gender: [],
+      age: [],
+      octave: [],
+      feels: []
+    };
+
+    rows.forEach(row => {
+      tags[row.type].push(row);
+    });
+
+    console.log(tags);
+
+    return res.render("../workspace/main.ejs", {
+      tags: tags
+    });
+  });
+});
+
+indexRouter.get("/view", (req, res, next) => {
+  res.render("../workspace/view.ejs", {
+    "abc": "def"
+  });
 });
 
 indexRouter.use("/board", boardRouter);
