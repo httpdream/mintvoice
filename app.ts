647cfa6e74ad3aaaa84efd69f6c8c4fc05865ce2
httpdream <httpdream.kay@gmail.com>
2018-03-27 18:35:31 +0900

    Fix file upload

--- app.ts
+++ app.ts
@@ -1,13 +1,38 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as multer from "multer";
+import { sql } from "./libs/db";
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    let keys = [req.body.category, req.body.gender, req.bod.octave];
+    keys = keys.concat(req.body.feels);
+    let t = file.originalname.split(".");
+    let extension = t[t.length - 1];
+    let fileName = keys.join("_") + extension;
+
+    sql.exec(`INSERT INTO voice (filename, original_filename) VALUES (?, ?)
+    `, [fileName, file.originalname])
+    .then (voice => {
+      console.log("insert voice", voice);
+      sql.exec(`
+      SELECT id, type
+      FROM tag
+      WHERE
+      key in ?
+      `, [keys]).then (async rows => {
+        for (let row of rows) {
+          await sql.exec(`INSERT INTO tag_voice (tag_id, tag_type, voice_id) VALUES (?, ?, ?)
+          `, [row.id, row.type, voice.id]);
+        }
+      });
+    })
+    .then(() => {
+      cb(null, fileName);
+    });
   }
 });
 
@@ -26,7 +51,7 @@ app.use(express.static("workspace"));
 app.use("/", indexRouter);
 
 app.post("/upload", upload.single("sampleFile"), (req, res, next) => {
-  res.send(req.file);
+  res.send(req["file"]);
 });
 
 app.use((err: Error, req, res, next) => {
