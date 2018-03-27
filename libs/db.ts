78dc28aa511c27f4d73d144306513bb09f93e9e6
kay <kay@yellotravel.com>
2018-03-27 22:20:58 +0900

    Use config

--- libs/db.ts
+++ libs/db.ts
@@ -1,12 +1,13 @@
 import mysql from "nodejs-mysql";
+import * as config from "config";
 
-const config = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "mint"
+const mysql_config = {
+  host: config.get("mysql.host"),
+  port: config.get("mysql.port"),
+  user: config.get("mysql.user"),
+  password: config.get("mysql.password"),
+  database: config.get("mysql.database")
 };
 
-const sql = mysql.getInstance(config);
+const sql = mysql.getInstance(mysql_config);
 export { sql };
