a53c0dc928406543f5e8917655b9416d7331eac9
httpdream <httpdream.kay@gmail.com>
2018-03-27 18:35:47 +0900

    Add get file upload

--- routes/index.ts
+++ routes/index.ts
@@ -34,6 +34,35 @@ indexRouter.get("/", (req, res, next) => {
   });
 });
 
+indexRouter.get("/upload", (req, res, next) => {
+  sql.exec(`
+  SELECT *
+  FROM tag`)
+  .then (rows => {
+    if (rows.length === 0) {
+      return res.render("../workspace/main.html");
+    }
+
+    let tags = {
+      category: [],
+      gender: [],
+      age: [],
+      octave: [],
+      feels: []
+    };
+
+    rows.forEach(row => {
+      tags[row.type].push(row);
+    });
+
+    console.log(tags);
+
+    return res.render("../workspace/uploadFile.ejs", {
+      tags: tags
+    });
+  });
+});
+
 indexRouter.get("/view", (req, res, next) => {
   res.render("../workspace/view.ejs", {
     "abc": "def"
