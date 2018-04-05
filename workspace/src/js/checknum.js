29e8d92bb7b73159d6d3c6bf6924121651312f1d
kay <kay@yellotravel.com>
2018-04-06 01:14:00 +0900

    Fix checkbox

--- workspace/src/js/checknum.js
+++ workspace/src/js/checknum.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   let selectNum = 0;
   $("#modalButton").click(function(){
-    selectNum = $('#modalCheckBox:checked').length;
+    selectNum = $('.modalCheckBox:checked').length;
     $("#selectNumber").text(selectNum);
   });
 });
