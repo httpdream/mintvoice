bc1c7a0e21f3ff4bf0f8c2f665151e9df7010836
ccj9707 <ccj9707@naver.com>
2018-04-10 18:38:42 +0900

    Mobile!

--- workspace/src/js/contact.js
+++ workspace/src/js/contact.js
@@ -1,4 +1,5 @@
 let boardSrc="#";
+let createDate=0;
 $(document).ready(function(){
   $.ajax({
     url: "/board/list?type=contact",
@@ -6,7 +7,7 @@ $(document).ready(function(){
     dataType: "JSON",
     success: response => {
       response.items.forEach(function(item, index){
-        let createDate = item.created_at.substring(5,10);
+        createDate = item.created_at.substring(5,10);
         $("#tableMain").append(
           "<tr><td><a href=\"" + boardSrc + "\">" + item.title + "</a></td><td>" + item.name + "</td><td>" + createDate + "</td><td>" + item.view + "</td></tr>"
         );
--- workspace/src/js/notice.js
+++ workspace/src/js/notice.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     dataType: "JSON",
     success: response => {
       response.items.forEach(function(item, index){
-        createDate = item.created_at.substring(0,10);
+        createDate = item.created_at.substring(5,10);
         $("#tableMain").append(
           "<tr><td><a href=\"" + boardSrc + "\">" + item.title + "</a></td><td>MintMedia</td><td>" + createDate + "</td><td>" + item.view + "</td></tr>"
         );
