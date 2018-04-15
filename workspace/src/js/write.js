e5b9c2ffff8f44a373f7fb734e64d3a5be786fd8
kay <kay@yellotravel.com>
2018-04-16 00:54:20 +0900

    Fix write content

--- workspace/src/js/write.js
+++ workspace/src/js/write.js
@@ -1,10 +1,15 @@
 $(() => {
   $("#write #submit").click(element => {
-    console.log($("#write").serializeArray());
+    let data = $("#write").serializeArray();
+    data = data.concat({
+      name: "content",
+      value: CKEDITOR.instances.editor.getData()
+    });
+    console.log(data);
     $.ajax({
       url: "/board",
       type: "POST",
-      data: $("#write").serializeArray(),
+      data: data,
       json: true,
       success: response => {
         console.log(response);
