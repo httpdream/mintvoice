e8236a6f15f872688673a1f80e7319e93fbe39d0
ccj9707 <34504466+ccj9707@users.noreply.github.com>
2018-04-16 00:52:25 +0900

    add WritingView

--- workspace/src/js/writingView.js
+++ workspace/src/js/writingView.js
@@ -1,17 +1,18 @@
 let boardSrc="#";
 let createDate=0;
+
 $(document).ready(function(){
   $.ajax({
-    url: "/board/list?type=contact",
+    url: "/board/view/"+board_id,
     type: "GET",
     dataType: "JSON",
     success: response => {
-      response.items.forEach(function(item, index){
-        createDate = item.created_at.substring(5,10);
-        $("#tableMain").append(
-          "<tr><td><a href=\"" + boardSrc + "\">" + item.title + "</a></td><td>" + item.name + "</td><td>" + createDate + "</td><td>" + item.view + "</td></tr>"
-        );
-      });
+      console.log(response.item);
+      createDate = response.item.created_at.substring(5,10);
+      $("#viewTitle").html(response.item.title);
+      $("#viewWriter").html(response.item.name);
+      $("#viewDate").html(createDate);
+      $("#viewMain").html(response.item.content);
     }
   });
 });
