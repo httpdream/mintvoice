2a2220b47846881b20fc430c41e41245ee51510a
ccj9707 <34504466+ccj9707@users.noreply.github.com>
2018-04-16 02:48:39 +0900

    writingView

--- workspace/src/js/writingView.js
+++ workspace/src/js/writingView.js
@@ -15,4 +15,44 @@ $(document).ready(function(){
       $("#viewMain").html(response.item.content);
     }
   });
+  preBoard();
+  nextBoard();
 });
+
+function preBoard(){
+  $.ajax({
+    url: "/board/view/"+(board_id-1),
+    type: "GET",
+    dataType: "JSON",
+    success: response => {
+      console.log(response.item);
+      if(!response.item.title){
+        $("#viewPre").html("&and;&nbsp;&nbsp;이전 글이 없습니다.");
+      }
+      else{
+        $("#viewPre").append(
+          "<a href=\"/board/"+ (board_id-1) + "\">" + response.item.title + "</a>"
+        );
+      }
+    }
+  });
+}
+
+function nextBoard(){
+  $.ajax({
+    url: "/board/view/"+(board_id+1),
+    type: "GET",
+    dataType: "JSON",
+    success: response => {
+      console.log(response.item);
+      if(!response.item.title){
+        $("#viewNext").html("&or;&nbsp;&nbsp;다음 글이 없습니다.");
+      }
+      else{
+        $("#viewNext").append(
+          "<a href=\"/board/"+ (board_id+1) + "\">" + response.item.title + "</a>"
+        )
+      }
+    }
+  });
+}
