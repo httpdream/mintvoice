9a7197fe22f508f5c663257f6f8efffb880cbc40
ccj9707 <34504466+ccj9707@users.noreply.github.com>
2018-05-02 03:24:59 +0900

    change design

---
+++ workspace/src/js/scrolling-nav.js
@@ -0,0 +1,26 @@
+(function($) {
+  "use strict";
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#textBar',
+    offset: 54
+  });
+
+})(jQuery);
